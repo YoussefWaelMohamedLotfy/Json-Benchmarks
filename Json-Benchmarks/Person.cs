39eb354946bb582b1071036ee38d4b585fd698d2
Yousef Wael <youssefwael.mohamedlotfy@se.com>
2024-03-25 01:03:22 +0200

    Add project files.

---
+++ Json-Benchmarks/Person.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Json_Benchmarks;
+
+internal sealed class Person
+{
+    public string Name { get; set; } = default!;
+
+    public int Age { get; set; }
+
+    public string? Address { get; set; }
+
+    //public Gender Gender { get; set; }
+
+    public List<string>? PhoneNumbers { get; set; }
+}
+
+public enum Gender
+{
+    None,
+    Male,
+    Female,
+    Other
+}
+
+[JsonSerializable(typeof(Person), GenerationMode = JsonSourceGenerationMode.Default)]
+internal partial class PersonJsonContext : JsonSerializerContext;
